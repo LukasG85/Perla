3936dd8f131015076074a3b74b632c6b79c05fe5
LukasG85 <lukas85b@gmail.com>
2019-01-31 12:20:05 +0100

    site v5

--- src/components/global/offer/OfferModal.js
+++ src/components/global/offer/OfferModal.js
@@ -65,18 +65,7 @@ export default class App extends React.Component {
             >
               {this.props.text}
             </p>
-            <button
-              className="modal-btn"
-              onClick={this.onCloseModal}
-              // style={{
-              //   fontSize: '1.5rem',
-              //   padding: '0.5rem 1rem',
-              //   border: 'none',
-              //   cursor: 'pointer',
-              //   margin: '2rem auto',
-              //   backgroundColor: `${styles.colors.mainYellow}`,
-              // }}
-            >
+            <button className="modal-btn" onClick={this.onCloseModal}>
               Zamknij
             </button>
           </div>
