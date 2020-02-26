6bba3da10e63b1c8583927a1f502689640b2e13a
LukasG85 <lukas85b@gmail.com>
2020-02-26 11:18:11 +0100

    robots.txt

--- gatsby-config.js
+++ gatsby-config.js
@@ -2,11 +2,21 @@ require('dotenv').config({
   path: `.env.${process.env.NODE_ENV}`,
 })
 
+const {
+  NODE_ENV,
+  URL: NETLIFY_SITE_URL = 'https://salon-perla.netlify.com/',
+  DEPLOY_PRIME_URL: NETLIFY_DEPLOY_URL = NETLIFY_SITE_URL,
+  CONTEXT: NETLIFY_ENV = NODE_ENV,
+} = process.env
+const isNetlifyProduction = NETLIFY_ENV === 'production'
+const siteUrl = isNetlifyProduction ? NETLIFY_SITE_URL : NETLIFY_DEPLOY_URL
+
 module.exports = {
   siteMetadata: {
     title: `Perła Jodłowa - Najlepszy salon fryzjerski w okolicy`,
     description: `Perła - Super ceny i mega zadowoleni klienci. Strzyżenie damskie, strzyżenie męskie, fryzury okolicznościowe, fryzury ślubne, koloryzacja, prostowanie, modelowanie, trwała ondulacja, sauna, prostowanie keratynowe i wiele innych. Zapraszamy do naszego salonu w Jodłowej`,
     author: `Łukasz Gancarz`,
+    siteUrl,
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
@@ -20,6 +30,7 @@ module.exports = {
 
     `gatsby-transformer-sharp`,
     `gatsby-plugin-sharp`,
+    `gatsby-plugin-sitemap`,
 
     {
       resolve: `gatsby-source-contentful`,
@@ -45,6 +56,27 @@ module.exports = {
         theme_color: `#000000`,
         display: `minimal-ui`,
         icon: `src/images/scissors.png`, // This path is relative to the root of the site.
+      },
+    },
+    {
+      resolve: 'gatsby-plugin-robots-txt',
+      options: {
+        resolveEnv: () => NETLIFY_ENV,
+        env: {
+          production: {
+            policy: [{ userAgent: '*' }],
+          },
+          'branch-deploy': {
+            policy: [{ userAgent: '*', disallow: ['/'] }],
+            sitemap: null,
+            host: null,
+          },
+          'deploy-preview': {
+            policy: [{ userAgent: '*', disallow: ['/'] }],
+            sitemap: null,
+            host: null,
+          },
+        },
       },
     },
     // this (optional) plugin enables Progressive Web App + Offline functionality
